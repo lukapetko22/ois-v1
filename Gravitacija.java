d03b2fa917e811894e72ec5b87b0d3d96d9336bf
Luka Petkovic <luka.petkovic2002@gmail.com>
2022-02-25 23:12:18 +0100

    Izpis gravitacijskega pospeška za nadmorsko višino iz ukazne vrstice

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,11 +1,14 @@
+import java.util.*;
+
 public class Gravitacija {
 	public static void main(String[] args) {
-		System.out.println("OIS je zakon!");
-		
-		double c=6.674*Math.pow(10,-11);
+	System.out.println("OIS je zakon!");
+	Scanner sc = new Scanner(System.in);
+	double v = sc.nextDouble();		
+	double c=6.674*Math.pow(10,-11);
     	double m=5.972*Math.pow(10,24);
     	double r=6.371*Math.pow(10,6);
-   		double a=(c*m)/(Math.pow(r+v,2));
+   	double a=(c*m)/(Math.pow(r+v,2));
 	System.out.println(r);
 	System.out.println(a);
 	
