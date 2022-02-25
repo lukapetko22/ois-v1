a91334e0af96b8e17a3b2e53bd94482d9e2adc18
irina-b1 <ib1158@student.uni-lj.si>
2022-02-25 22:51:52 +0100

    dodaj izpis pospeska #3

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,5 +1,11 @@
 public class Gravitacija {
 	public static void main(String[] args) {
 		System.out.println("OIS je zakon!");
+		
+		double c=6.674*Math.pow(10,-11);
+    	double m=5.972*Math.pow(10,24);
+    	double r=6.371*Math.pow(10,6);
+   		double a=(c*m)/(Math.pow(r+v,2));
+	
 	}
 }
